2e8398e8d93cef9ac16b84c68ab0b2c2edbed00b
MSI\81804 <a1929776588@gmail.com>
2022-06-22 22:03:44 +0900

    スタート画面の走ってる奴らが走らなかったからはしらせた

--- Assets/Script/RunningMan.cs
+++ Assets/Script/RunningMan.cs
@@ -7,19 +7,23 @@ public class RunningMan : MonoBehaviour
     [SerializeField][Tooltip("移動速度")] float _speed = -2.0f;
     [Tooltip("空のアニメーターの置き場")] private Animator anim;
     [Tooltip("右の端の場所")] private Transform _Xposition;
+    [SerializeField][Tooltip("召喚間隔")] private float _targetTime = default;
+    [SerializeField][Tooltip("召喚間隔数えるマン")] private float _currentTime = default;
     private void Start()
     {
         anim = GetComponent<Animator>();
         _Xposition = GetComponent<Transform>();
+        GetComponent<Rigidbody2D>().velocity = new Vector2(_speed, 0);
     }
 
     void Update()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(_speed, 0);
-        if (transform.position.x < -1)
+        _currentTime += Time.deltaTime;
+
+        if (_targetTime < _currentTime)
         {
-            Destroy(gameObject);
-            //transform.position = new Vector2(21, _Xposition.position.y);
+            transform.position = new Vector2(21, _Xposition.position.y);
+            _currentTime = 0;
         }
     }
 }
